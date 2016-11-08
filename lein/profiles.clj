fa163142256c6732a7f977c277042f3ac6ab4d89
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-11-08 19:10:39 +0200

    Add lein configuration

---
+++ lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:ultra {:color-scheme :solarized_dark
+                :stacktraces  true}
+        :plugins [[lein-try "0.4.3"]
+                  [lein-ancient "0.6.10-SNAPSHOT"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.3"]
+                  ;[venantius/ultra "0.5.0"]
+                 ]}}
