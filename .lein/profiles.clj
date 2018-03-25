56771bee131a6a07021dce0fb1fa7e93bb7e1564
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-03-25 04:14:53 +0300

    Fix location for lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,13 @@
 {:user {:ultra {:color-scheme :solarized_dark
-                :stacktraces  true}
+                :stacktraces  true
+                :repl true
+                :java true
+                :tests true}
         :plugins [[lein-try "0.4.3"]
                   [lein-ancient "0.6.10-SNAPSHOT"]
                   [lein-kibit "0.1.2"]
                   [jonase/eastwood "0.2.3"]
-                  ;[venantius/ultra "0.5.0"]
-                 ]}}
+                  ;[venantius/ultra "0.5.1"]
+                 ]
+        :repl-options {:init (set! *print-length* 42)}
+	}}
