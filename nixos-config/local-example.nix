7851413f1b3fd67ed2703970ef03ce5d03259162
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-04-01 12:13:14 +0300

    Nix: make local-example more comprehensive

--- nixos-config/local-example.nix
+++ nixos-config/local-example.nix
@@ -5,18 +5,31 @@
     ./server.nix
   ];
 
-  # UEFI
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
 
   # Network and Firewall
   networking = {
     hostName = "put-hostname-here";
+
     firewall = {
       enable = true;
       allowedTCPPorts = [ 10000 ];
     };
-    
+
+    useDHCP = false;
+    interfaces.ens45.ipv4 = {
+      addresses = [{
+        address = "64.42.64.42";
+        prefixLength = 24;
+      }];
+      routes = [
+        # Default route
+        { prefixLength = 0; address = "0.0.0.0"; via = "64.42.64.1"; }
+      ];
+    };
+
     # resolv.conf
     nameservers = [
       "8.8.8.8"
