7b7a2ffafe3b67c5d6469adeb9d29e212e660cf6
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-08-11 15:34:43 +0300

    More updates from trajan

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,11 +3,8 @@
                 :repl true
                 :java true
                 :tests true}
-        :plugins [[lein-try "0.4.3"]
-                  [lein-ancient "0.6.10-SNAPSHOT"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.3"]
-                  ;[venantius/ultra "0.5.1"]
-                 ]
+        :plugins [[lein-ancient "0.6.15"]]
         :repl-options {:init (set! *print-length* 42)}
-	}}
+        :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
