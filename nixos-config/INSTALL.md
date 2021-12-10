40952484d19c46495f4ab4f2b1a84276212a230b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2021-12-10 19:41:56 +0200

    Add duckdns to tiberius

---
+++ nixos-config/INSTALL.md
@@ -0,0 +1,4 @@
+Really a mix of
+- https://elis.nu/blog/2019/08/encrypted-zfs-mirror-with-mirrored-boot-on-nixos/
+- https://nixos.org/manual/nixos/stable/index.html#sec-installation
+- https://github.com/turlando/antigone/blob/master/install.txt#L118
--- nixos-config/tiberius.nix
+++ nixos-config/tiberius.nix
@@ -2,23 +2,24 @@
 
 {
   # Boot config
+  # See: https://discourse.nixos.org/t/nixos-on-mirrored-ssd-boot-swap-native-encrypted-zfs/9215/5
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.zfsSupport = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.mirroredBoots = [
+    { devices = ["nodev"]; path = "/boot"; }
+    { devices = ["nodev"]; path = "/boot-fallback"; }
+  ];
+  boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
-  boot.loader.grub.mirroredBoots = [ { devices = ["nodev"]; path = "/boot"; } { devices = ["nodev"]; path = "/boot-fallback"; } ];
-  boot.supportedFilesystems = [ "zfs" ]; 
 
-  # See https://discourse.nixos.org/t/nixos-using-integrated-gpu-for-display-and-external-gpu-for-compute-a-guide/12345
-  environment.systemPackages = [ pkgs.linuxPackages.nvidia_x11 ];
-  boot.extraModulePackages = [ pkgs.linuxPackages.nvidia_x11 ];
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia_drm" "nvidia_modeset" "nvidia" ];
-
+  # This is for sensors to work
   boot.kernelModules = ["coretemp" "it87"];
+
   # ZFS config
   services.zfs = {
-    autoSnapshot = { 
+    autoSnapshot = {
       enable = true;
       monthly = 1; # keep only one monthly instead of 12
     };
@@ -28,23 +29,54 @@
     };
   };
 
-  networking.hostName = "tiberius";  
+  networking.hostName = "tiberius";
   networking.firewall.enable = false;
   networking.hostId = "98546379";
   networking.enableIPv6 = false;
-  networking.defaultGateway = { address = "192.168.8.1"; interface = "enp2s0"; metric = 10; };
+  networking.defaultGateway = {
+    address = "192.168.8.1";
+    interface = "enp2s0";
+    metric = 10;
+  };
   networking.dhcpcd.extraConfig =
   ''
     interface enp1s0
     metric 300
-    
+
     interface enp2s0
     metric 200
-  '';  
+  '';
+  # TODO: investigate this if needed: https://askubuntu.com/questions/1134115/kvm-guest-cant-access-internet
 
   users.users.fabrizio.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6UcGI3prraCqsgl/A47zLxV15ZfAg0dDzB1C9b8RKC fabrizio+tiberius@caesar"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZLEPCkzxJwC5nEtDWyMatLnQYKVe/V2fbP/iIBeecj fabrizio+tiberius@augustus"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFbvK96xBIc8OH147+Kv7k/0DG1HeHSngD6Vcgx4OwW fabrizio+tiberius@claudius"
   ];
+
+  systemd.services.duckdns= {
+    description = "Ping DuckDNS";
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "fabrizio";
+      Group = "fabrizio";
+      Environment = ''HEALTHCHECKS_TOKEN=${(import ./secrets.nix).healthchecksToken}'';
+      Environment = ''DUCKDNS_TOKEN=${(import ./secrets.nix).duckdnsToken}'';
+      ExecStart = "/home/fabrizio/bin/duckdns";
+    };
+    # path = with pkgs; [ bash curl ];
+  };
+  systemd.timers.duckdns = {
+    description = "Ping DuckDNS";
+    wantedBy = [ "timers.target" ];
+    partOf = [ "duckdns.service" ];
+    timerConfig = {
+      OnBootSec = "2min";
+      OnUnitActiveSec = "120min";
+      Unit = "duckdns.service";
+    };
+  };
+  # systemd.services.duckdns.enable = true;
+  # systemd.timers.duckdns.enable = true;
 }
