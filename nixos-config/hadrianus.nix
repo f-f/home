1b940762b99a83e4f558736dba12046f5fea8aac
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-03-07 23:38:47 +0200

    Update mac

--- nixos-config/hadrianus.nix
+++ nixos-config/hadrianus.nix
@@ -1,4 +1,87 @@
 { pkgs, ... }:
+let
+  brewPkgs = [
+    "magic-wormhole"
+    "libvirt"
+    "virt-manager"
+    "virt-viewer"
+  ];
+  brewCasks = [
+    "ableton-live-suite"
+    "audacity"
+    "caffeine"
+    "calibre"
+    "crossover"
+    "discord"
+    "dropbox"
+    "firefox"
+    "fl-studio"
+    "google-chrome"
+    "iina"
+    "inkscape"
+    "keepassxc"
+    "kitty"
+    "krita"
+    "launchcontrol"
+    "little-snitch"
+    "loopback"
+    "mailspring"
+    "microsoft-outlook"
+    "native-access"
+    "obsidian"
+    "plugdata"
+    "rectangle"
+    "secretive"
+    "skype"
+    "slack"
+    "splice"
+    "spotify"
+    "stats"
+    "steam"
+    "telegram"
+    "vcv-rack"
+    "visual-studio-code"
+    "vlc"
+    "wireshark"
+    "xournal-plus-plus"
+    "zoom"
+  ];
+  nixPkgs = with pkgs; [
+    arp-scan
+    awscli2
+    bat
+    clang
+    coreutils
+    curl
+    diff-so-fancy
+    fd
+    (ffmpeg-full.override {game-music-emu = null;})
+    fzf
+    git-lfs
+    git-filter-repo
+    gnumake
+    gnupg
+    iftop
+    jc
+    jq
+    lorri
+    m-cli
+    nmap
+    nodejs
+    python3
+    ripgrep
+    rsync
+    silver-searcher
+    terminal-notifier
+    tokei
+    tldr
+    tree
+    wget
+    youtube-dl
+  ];
+  secrets = "/Users/fabrizio/nixos-config/secrets.sh";
+in
+
 {
   networking.hostName = "hadrianus";
 
@@ -19,6 +102,35 @@
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
 
+  launchd =
+    let
+      runEvery = StartInterval: {
+        inherit StartInterval;
+        Nice = 5;
+        LowPriorityIO = true;
+        AbandonProcessGroup = true;
+      };
+      runCommand = command: {
+        inherit command;
+        serviceConfig.RunAtLoad = true;
+        serviceConfig.KeepAlive = true;
+      }; in {
+
+    user.agents = {
+      obsidianBackup = {
+        script = ''
+          source ${secrets}
+          /Users/fabrizio/bin/zk-backup
+        '';
+        serviceConfig = (runEvery 86400) // { RunAtLoad = true; StandardOutPath = "/Users/fabrizio/backstdout.log"; StandardErrorPath = "/Users/fabrizio/backstderr.log"; };
+      };
+      cleanDownloads = {
+        script = "/Users/fabrizio/bin/cleanup-downloads";
+        serviceConfig = (runEvery 86400) // { RunAtLoad = true; };
+      };
+    };
+  };
+
   homebrew = {
     enable = true;
     global.brewfile = true;
@@ -34,49 +146,8 @@
       "homebrew/cask-drivers"
       "f-f/homebrew-virt-manager"
     ];
-    brews = [
-      "magic-wormhole"
-      "libvirt"
-      "virt-manager"
-      "virt-viewer"
-    ];
-    casks = [
-      "ableton-live-suite"
-      "audacity"
-      "caffeine"
-      "calibre"
-      "crossover"
-      "discord"
-      "dropbox"
-      "firefox"
-      "fl-studio"
-      "google-chrome"
-      "iina"
-      "inkscape"
-      "keepassxc"
-      "kitty"
-      "krita"
-      "little-snitch"
-      "loopback"
-      "mailspring"
-      "microsoft-outlook"
-      "native-access"
-      "obsidian"
-      "plugdata"
-      "rectangle"
-      "secretive"
-      "skype"
-      "slack"
-      "splice"
-      "spotify"
-      "stats"
-      "steam"
-      "telegram"
-      "vcv-rack"
-      "visual-studio-code"
-      "vlc"
-      "zoom"
-    ];
+    brews = brewPkgs;
+    casks = brewCasks;
     masApps = {
       # Xcode = 497799835;
     };
@@ -88,38 +159,7 @@
     users.fabrizio = { pkgs, ... }: {
       home.stateVersion = "22.05";
 
-      home.packages = with pkgs; [
-        arp-scan
-        awscli2
-        bat
-        clang
-        coreutils
-        curl
-        diff-so-fancy
-        fd
-        (ffmpeg-full.override {game-music-emu = null;})
-        fzf
-        git-lfs
-        git-filter-repo
-        gnumake
-        gnupg
-        iftop
-        jc
-        jq
-        lorri
-        m-cli
-        nmap
-        nodejs
-        python3
-        ripgrep
-        rsync
-        silver-searcher
-        terminal-notifier
-        tokei
-        tldr
-        wget
-        youtube-dl
-      ];
+      home.packages = nixPkgs;
 
       # https://github.com/nix-community/home-manager/issues/423
       home.sessionVariables = {
